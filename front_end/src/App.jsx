3c8842c260cffebd88aac849f29d3fc8b502f3f1
ousman <ousmanmuhammed137@gmail.com>
2025-04-10 22:50:58 -0900

    bing bang

---
+++ front_end/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=''>
+     <p className=' text-3xl text-red-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad mollitia optio nihil, iure praesentium facere ipsa fugiat porro, natus exercitationem ullam, dolorem consequatur quidem culpa iste obcaecati quisquam. Laudantium, suscipit!</p>
+    </div>
+  )
+}
+
+export default App
