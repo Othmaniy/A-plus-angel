6cea43f567c77d8b6bb60bc68bdd02635cd4416f
ousman <ousmanmuhammed137@gmail.com>
2025-04-18 21:47:09 -0900

    router done on app jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,12 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import UserDashboard from "./pages/UserDashbard";
+
 function App() {
   return (
-    <>
-      <Header/>
-      <Home />
-     
-    </>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<UserDashboard />} />
+          {/* <Route path="/contact" element={<Home />} /> */}
+        </Routes>
+      </>
+    </Router>
   );
 }
+
 export default App;
